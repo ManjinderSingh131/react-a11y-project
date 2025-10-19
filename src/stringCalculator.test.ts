6eeac1e89c3ce2836764b5ada31720115ebde9ad
ManjinderSingh131 <smanjinder576@gmail.com>
2025-10-19 18:45:25 +0530

    feat: stringcalculator handle decimal numbers

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -3,8 +3,8 @@ import {describe, expect, test} from 'vitest';
 const stringCalculator = (input: string): number => {
     if (input === "") return 0;
     input = input.replace(/\n/g, ',');
-    input = input.replace(/[^0-9,]/g, '');
-    const numbers = input.split(',').map(Number);
+    input = input.replace(/[^0-9,.]/g, '');
+    const numbers = input.split(',').map(parseFloat).filter(num => !isNaN(num));
     return numbers.reduce((sum, num) => sum + num, 0);
 }
 
